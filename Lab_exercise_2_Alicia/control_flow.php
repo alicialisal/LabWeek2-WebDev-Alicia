74836a5e86240543dd7c881f05d979d89af4dda7
alicialisal <ajuanita@student.ciputra.ac.id>
2024-10-03 09:45:38 +0800

    update

--- Lab_exercise_2_Alicia/control_flow.php
+++ Lab_exercise_2_Alicia/control_flow.php
@@ -2,8 +2,8 @@
 
 class Control_flow {
     function checkAge($age) {
-        if ($age >= 18) echo "You are an adult<br>";
-        else echo "You are a minor<br>";
+        if ($age >= 18) echo "You are an adult.<br>";
+        else echo "You are a minor.<br>";
     }
 }
 
--- Lab_exercise_2_Alicia/functions_array.php
+++ Lab_exercise_2_Alicia/functions_array.php
@@ -3,7 +3,7 @@
 class function_array {
     function greet($students) {
         foreach($students as $name)
-            echo "Hello $name<br>";
+            echo "Hello $name!<br>";
     }
 }
 
